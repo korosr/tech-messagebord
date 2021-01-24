8d16990ef1a0e3a9b358e5319cec960dfdcd9631
korosr <kaoriisa1206@gmail.com>
2021-01-24 09:58:13 +0900

    基本機能の追加

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,16 +14,16 @@ import model.Message;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet(name = "index", urlPatterns = { "/index" })
-public class IndexServlet extends HttpServlet {
+@WebServlet(name = "show", urlPatterns = { "/show" })
+public class ShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public ShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,13 +34,22 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EntityManager em = DBUtil.createEntityManager();
 
-		List<Message>  messages = em.createNamedQuery("getAllMessages" , Message.class).getResultList();
+		Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
+
 		em.close();
 
-		request.setAttribute("messages", messages);
+		request.setAttribute("message", m);
 
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
 		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
