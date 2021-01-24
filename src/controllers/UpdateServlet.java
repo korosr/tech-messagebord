929df32e3e5fb52ef04889ea9461227bd0f94633
korosr <kaoriisa1206@gmail.com>
2021-01-24 12:16:25 +0900

    css,validation,nullこ考慮追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Message;
+import model.validators.MessageValidator;
 import utils.DBUtil;
 
 /**
@@ -20,13 +23,13 @@ import utils.DBUtil;
 public class UpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UpdateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UpdateServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -45,30 +48,43 @@ public class UpdateServlet extends HttpServlet {
 		if(_token != null && _token.equals(request.getSession().getId())) {
 			EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+			// セッションスコープからメッセージのIDを取得して
+			// 該当のIDのメッセージ1件のみをデータベースから取得
+			Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
-            // フォームの内容を各フィールドに上書き
-            String title = request.getParameter("title");
-            m.setTitle(title);
+			// フォームの内容を各フィールドに上書き
+			String title = request.getParameter("title");
+			m.setTitle(title);
 
-            String content = request.getParameter("content");
-            m.setContent(content);
+			String content = request.getParameter("content");
+			m.setContent(content);
 
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+			Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+			m.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
-            // データベースを更新
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            em.close();
+			//バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+			List<String> errors = MessageValidator.validate(m);
+			if(errors.size() > 0) {
+				em.close();
+				request.setAttribute("_token", request.getSession().getId());
+				request.setAttribute("message", m);
+				request.setAttribute("errors", errors);
 
-            // セッションスコープ上の不要になったデータを削除
-            request.getSession().removeAttribute("message_id");
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+				rd.forward(request, response);
+			}else {
+				// データベースを更新
+				em.getTransaction().begin();
+				em.getTransaction().commit();
+				request.getSession().setAttribute("flush", "更新が完了しました。");
+				em.close();
 
-            // indexページへリダイレクト
-            response.sendRedirect(request.getContextPath() + "/index");
+				// セッションスコープ上の不要になったデータを削除
+				request.getSession().removeAttribute("message_id");
+
+				// indexページへリダイレクト
+				response.sendRedirect(request.getContextPath() + "/index");
+			}
 		}
 	}
 }
